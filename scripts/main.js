a992007d168029fe5c500224c844801f6195abe0
EricKugel <erickugel713@gmail.com>
2022-01-28 19:02:35 -0600

    Sort of did character states/animation

--- scripts/main.js
+++ scripts/main.js
@@ -39,27 +39,35 @@ class Character {
         this.tick = 0;
         this.x = 200;
         this.y = 200;
+        this.state = "Idle";
+        this.direction = "forward";
     }
 
     update() {
+        this.state = "Walk"
         if (keysDown["ArrowRight"]) {
+            this.direction = "right";
             this.x += 5;
-        } if (keysDown["ArrowLeft"]) {
+        } else if (keysDown["ArrowLeft"]) {
+            this.direction = "left";
             this.x -= 5;
-        } if (keysDown["ArrowUp"]) {
+        } else if (keysDown["ArrowUp"]) {
+            this.direction = "backward";
             this.y -= 5;
-        } if (keysDown["ArrowDown"]) {
+        } else if (keysDown["ArrowDown"]) {
+            this.direction = "forward";
             this.y += 5;
+        } else {
+            this.state = "Idle"
         }
         this.tick += 1;
-        this.tick = this.tick % 1000;
     }
 
     draw() {
         if (this.images == undefined) {
             this.images = imageCache;
         }
-        var imageName = "characters/" + this.name + "/" + this.name + String((Math.floor(this.tick / 10)) % 3);
+        var imageName = "characters/" + this.name + "/" + this.direction + this.state + (this.tick % 8);
         let image = this.images[imageName];
         ctx.drawImage(image, this.x, this.y);
     }
